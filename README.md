5ae1565dfc9ad6e6657fcec46aff46407be0e7c6
Jim Doyle <indellibleink@gmail.com>
2018-05-10 18:46:25 -0400

    Updated files

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # JavaScript-DOM
 
-JavaScript & the DOM lesson: lesson 3
 JavaScript & the DOM lesson: lesson 4
--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-var titles=document.getElementsByClassName('title');
-Array.from(titles).forEach(function(item){
-  console.log(item);
+const wmf=document.querySelector('#book-list li:nth-child(2) .name');
+// console.log(wmf);
+
+ var books=document.querySelector('#book-list li .name');
+ //console.log(books);
+
+ books= document.querySelectorAll('#book-list li .name');
+ console.log(books);
+
+Array.from(books).forEach(function(book){
+  console.log(book);
 })
