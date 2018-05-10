a9e0d7bf2ff3b1f9b69544c8441bb3f88dae45af
jfdoyle3 <indellibleink@gmail.com>
2018-05-10 01:53:18 +0000

    Updated Script

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
-const wrap=document.query.Selector('#wrapper');
-console.log(wrap);
+const search = document.getElementById('search-books');
+const bookList = document.getElementById('book-list');
+
+console.log(search, bookList);
