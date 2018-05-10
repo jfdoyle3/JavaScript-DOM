2cb94c588055c3ef0c468ce3125101b8719ed651
Jim Doyle <indellibleink@gmail.com>
2018-05-10 19:34:10 -0400

    updated code

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-const wmf=document.querySelector('#book-list li:nth-child(2) .name');
-// console.log(wmf);
+var books=document.querySelectorAll('#book-list li .name');
+books.forEach(function(book){
+   book.textContent +=' (Book Title)';
 
- var books=document.querySelector('#book-list li .name');
- //console.log(books);
+});
 
- books= document.querySelectorAll('#book-list li .name');
- console.log(books);
-
-Array.from(books).forEach(function(book){
-  console.log(book);
-})
+const bookList=document.querySelector('#book-list');
+// bookList.innerHTML='<h2>Books and more books...</h2>';
+bookList.innerHTML += '<p>This is how you add HTML';
