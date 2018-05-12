de999f4ed08471c85f034dec0f441610c9321d83
Jim Doyle <indellibleink@gmail.com>
2018-05-12 10:44:38 -0400

    lesson 9 code

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const bookList=document.querySelector('#book-list');
-console.log('book-list next sibling is: ',bookList.nextSibling);
-console.log('book-list next element sibling is: ',bookList.nextElementSibling);
-
-console.log('book-list previous sibling is: ',bookList.previousSibling);
-console.log('book-list previous element sibling is: ',bookList.previousElementSibling);
-
-bookList.previousElementSibling.querySelector('p').innerHTML += '<br/> Too cool for everyone else';
+var btns=document.querySelectorAll('#book-list .delete');
+Array.from(btns).forEach(function(btn){
+  btn.addEventListener('click',function(e){
+    const li=e.target.parentElement;
+    li.parentNode.removeChild(li)
+  });
+});
+const link=document.querySelector('#page-banner a');
+link.addEventListener('click',function(e){
+  e.preventDefault();
+  console.log("navigaton to ", e.target.textContent,' was prevented');
+})
