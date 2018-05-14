6ca7cb3127bc178857cef4cb7b4c51d6fd5211a9
Jim Doyle <indellibleink@gmail.com>
2018-05-14 05:07:59 -0400

    updated files

---
+++ app.js
@@ -0,0 +1,45 @@
+const list=document.querySelector('#book-list ul');
+//delete books
+list.addEventListener('click',function(e){
+  if (e.target.className == 'delete'){
+    const li=e.target.parentElement;
+    // li.parentNode.removeChild(li);
+       list.removeChild(li);
+  }
+});
+
+//add book
+const addForm=document.forms['add-book'];
+addForm.addEventListener('submit', function(e){
+  e.preventDefault();
+  const value=addForm.querySelector('input[Type="text"]').value;
+
+  // create elements
+  const li = document.createElement('li');
+  const bookName=document.createElement('span');
+  const deleteBtn=document.createElement('span');
+
+  //add content
+  deleteBtn.textContent ='delete';
+  bookName.textContent= value;
+
+  //add classes
+  bookName.classList.add('name');
+  deleteBtn.classList.add('delete');
+
+  // append to document
+  li.appendChild(bookName);
+  li.appendChild(deleteBtn);
+  list.appendChild(li);
+
+});
+
+//hide Books
+const hideBox=document.querySelector('#hide');
+hideBox.addEventListener('change', function(e){
+  if (hideBox.checked){
+    list.style.display='none';
+  }else{
+    list.style.display='initial';
+  }
+})
